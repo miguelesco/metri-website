6a1815bfcd39a2cfd85547d978438d82d052cb4f
Miguelangel Escorche <miguelangelescorche@gmail.com>
2024-05-24 18:24:51 -0500

    table responsive

--- layouts/components/userInfo.tsx
+++ layouts/components/userInfo.tsx
@@ -103,111 +103,123 @@ const UserInfo: React.FC<UserInfoProps> = ({chartOptions}) => {
 
 					</div>
 				</section>
-				<section className="flex-none flex flex-col space-y-8 mt-5 2xl:relative 2xl:bottom-[291px] 2xl:w-[830px]">
-					<h2 className="font-semibold text-xl sm:text-2xl">Transaction History</h2>
-                <table className="w-full" cellPadding={0} border={0}>
-					<thead>
-						<tr>
-							<th className="px-4 py-2"></th>
-							<th className="px-4 py-2">Status</th>
-							<th className="px-4 py-2">Date</th>
-							<th className="px-4 py-2">Amount</th>
-							<th className="px-4 py-2">Actions</th>
-						</tr>
-					</thead>
-					<tbody className="divide-y-8 divide-transparent w-full h-60 md:table-row-group space-y-4">
-						{
-							paginatedData?.map((history, index) => (
-								<tr
-								key={index}
-								className="w-full relative grid grid-cols-3 grid-rows-2 md:table-row items-center gap-x-2 "
-								style={{ gridTemplateColumns: '1fr 6fr 2fr' }}
-							>
-								<td className="col-span-1 row-span-2 h-11">
-									{history.status === 'completed' ? <IoCheckmarkDoneOutline/> : <FaArrowsRotate/>}
-								</td>
-								<td className="col-start-2 col-end-3 row-start-1 row-end-1 h-11">
-									<span
-										className="font-medium text-lg sm:text-lg tracking-tight text-gray-800 leading-none"
-										>{history.status}</span
-									>
-								</td>
-								<td className="col-start-2 col-end-3 row-start-2 row-end-2 h-11">
-									<span
-										className="font-normal text-base tracking-tight text-gray-500 leading-none"
-										>{new Date(history.created_at).toLocaleString()}</span
-									>
-								</td>
-								<td
-									className="col-start-3 col-end-4 row-start-1 row-end-3 text-right place-items-center h-11"
-								>
-									<span
-										className="font-medium text-lg sm:text-lg tracking-tight text-gray-800"
-										>{history.amount} <span className="text-xs">MTR</span></span
-									>
-								</td>
-								<td className=" col-start-3 col-end-4 row-start-1 row-end-3 text-right place-items-center justify-center gap-4 h-11">
-									
-									<div className="flex justify-center gap-2">
-										
-										<CopyToClipboard text={history.transaction_id} onCopy={() => handleCopy(index)}>
-											<button className="outline-2 outline-slate-950 group cursor-pointer relative flex gap-1.5 px-4 py-2 bg-lightGreen bg-opacity-15 text-darkGreen text-opacity-70 rounded-2xl hover:bg-opacity-70 transition font-semibold shadow-md">
-												Copy ID
-												{visibleIndex === index && (
-													<span className="pointer-events-none absolute opacity-0 group-focus:opacity-100 group-focus:text-gray-700 group-focus:text-sm group-focus:-translate-y-12 duration-700">
-														Copied to clipboard
-													</span>
-												)}
-											</button>
-										</CopyToClipboard>
+				<section className="flex flex-col space-y-8 mt-5 2xl:relative 2xl:bottom-[291px] 2xl:w-[830px]  overflow-x-auto">
+						<h2 className="font-semibold text-xl sm:text-2xl">Transaction History</h2>
+						<div className="relative overflow-x-auto">
+							<table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400" cellPadding={0} border={0}>
+								<thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+									<tr>
+										<th scope="col" className="px-6 py-3">
+											
+										</th>
+										<th scope="col" className="px-6 py-3">
+											Status
+										</th>
+										<th scope="col" className="px-6 py-3">
+											Date
+										</th>
+										<th scope="col" className="px-6 py-3">
+											Amount
+										</th>
+										<th scope="col" className="px-6 py-3">
+											Actions
+										</th>
+									</tr>
+								</thead>
+								<tbody className="divide-y-8 divide-transparent w-full h-60 md:table-row-group space-y-4">
+									{
+										paginatedData?.map((history, index) => (
+											<tr
+											key={index}
+											className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+											style={{ gridTemplateColumns: '1fr 6fr 2fr' }}
+										>
+											<th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+												{history.status === 'completed' ? <IoCheckmarkDoneOutline/> : <FaArrowsRotate/>}
+											</th>
+											<td className="px-6 py-4">
+												<span
+													className="font-medium text-lg sm:text-lg tracking-tight text-gray-800 leading-none"
+													>{history.status}</span
+												>
+											</td>
+											<td className="px-6 py-4">
+												<span
+													className="font-normal text-base tracking-tight text-gray-500 leading-none"
+													>{new Date(history.created_at).toLocaleString()}</span
+												>
+											</td>
+											<td
+												className="px-6 py-4"
+											>
+												<span
+													className="font-medium text-lg sm:text-lg tracking-tight text-gray-800"
+													>{history.amount} <span className="text-xs">MTR</span></span
+												>
+											</td>
+											<td className=" px-6 py-4">
+												
+												<div className="flex justify-center gap-2">
+													
+													<CopyToClipboard text={history.transaction_id} onCopy={() => handleCopy(index)}>
+														<button className="outline-2 outline-slate-950 group cursor-pointer relative flex gap-1.5 px-4 py-2 bg-lightGreen bg-opacity-15 text-darkGreen text-opacity-70 rounded-2xl hover:bg-opacity-70 transition font-semibold shadow-md">
+															Copy ID
+															{visibleIndex === index && (
+																<span className="pointer-events-none absolute opacity-0 group-focus:opacity-100 group-focus:text-gray-700 group-focus:text-sm group-focus:-translate-y-12 duration-700">
+																	Copied to clipboard
+																</span>
+															)}
+														</button>
+													</CopyToClipboard>
 
-										
-										{history.status === 'completed' && history.transaction_hash && (
-										<CopyToClipboard text={history.transaction_hash} onCopy={() => handleCopy(index)}>
-											<button className="outline-2 outline-slate-950 group cursor-pointer relative flex gap-1.5 px-4 py-2 bg-lightGreen bg-opacity-15 text-darkGreen text-opacity-70 rounded-2xl hover:bg-opacity-70 transition font-semibold shadow-md">
-												Copy Hash
-												{visibleIndex === index && (
-													<span className="pointer-events-none absolute opacity-0 group-focus:opacity-100 group-focus:text-gray-700 group-focus:text-sm group-focus:-translate-y-12 duration-700">
-														Copied to clipboard
-													</span>
-												)}
-											</button>
-										</CopyToClipboard>
-										)}
-									</div>
-								</td>
-							</tr>
-							))
-						}
-					</tbody>
-				</table>
-				<div className="flex justify-between mt-4">
-					<button
-					onClick={handlePrevPage}
-					disabled={currentPage === 0}
-					className={`px-4 py-2 bg-gray-200 text-gray-700 rounded ${currentPage === 0 && 'opacity-50 cursor-not-allowed'}`}
-					>
-					Previous
-					</button>
-					<div className="flex space-x-2">
-					{Array.from({ length: totalPages }, (_, index) => (
-						<button
-						key={index}
-						onClick={() => handlePageClick(index)}
-						className={`px-4 py-2 ${currentPage === index ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'} rounded`}
-						>
-						{index + 1}
-						</button>
-					))}
-					</div>
-					<button
-					onClick={handleNextPage}
-					disabled={currentPage === totalPages - 1}
-					className={`px-4 py-2 bg-gray-200 text-gray-700 rounded ${currentPage === totalPages - 1 && 'opacity-50 cursor-not-allowed'}`}
-					>
-					Next
-					</button>
-				</div>
+													
+													{history.status === 'completed' && history.transaction_hash && (
+													<CopyToClipboard text={history.transaction_hash} onCopy={() => handleCopy(index)}>
+														<button className="outline-2 outline-slate-950 group cursor-pointer relative flex gap-1.5 px-4 py-2 bg-lightGreen bg-opacity-15 text-darkGreen text-opacity-70 rounded-2xl hover:bg-opacity-70 transition font-semibold shadow-md">
+															Copy Hash
+															{visibleIndex === index && (
+																<span className="pointer-events-none absolute opacity-0 group-focus:opacity-100 group-focus:text-gray-700 group-focus:text-sm group-focus:-translate-y-12 duration-700">
+																	Copied to clipboard
+																</span>
+															)}
+														</button>
+													</CopyToClipboard>
+													)}
+												</div>
+											</td>
+										</tr>
+										))
+									}
+								</tbody>
+							</table>
+							{/* <div className="flex justify-between mt-4" id="arrows">
+								<button
+								onClick={handlePrevPage}
+								disabled={currentPage === 0}
+								className={`px-4 py-2 bg-gray-200 text-gray-700 rounded ${currentPage === 0 && 'opacity-50 cursor-not-allowed'}`}
+								>
+								Previous
+								</button>
+								<div className="flex space-x-2">
+								{Array.from({ length: totalPages }, (_, index) => (
+									<button
+									key={index}
+									onClick={() => handlePageClick(index)}
+									className={`px-4 py-2 ${currentPage === index ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-700'} rounded`}
+									>
+									{index + 1}
+									</button>
+								))}
+								</div>
+								<button
+								onClick={handleNextPage}
+								disabled={currentPage === totalPages - 1}
+								className={`px-4 py-2 bg-gray-200 text-gray-700 rounded ${currentPage === totalPages - 1 && 'opacity-50 cursor-not-allowed'}`}
+								>
+								Next
+								</button>
+							</div> */}
+						</div>
 				</section> 
 				
 			</div> 
